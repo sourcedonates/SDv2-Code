e316079cc58b6ec76ae2ad39b1b7a3b0e636a5bc
Werner 'Arrow768 <arrow768@sourcedonates.com>
2014-08-16 17:25:34 +0200

    Added basic layout for the queue worker

--- app/workers/PaymentQueueWorker.php
+++ app/workers/PaymentQueueWorker.php
@@ -4,6 +4,35 @@ class PaymentQueueWorker
 {
     public function fire($job, $data)
     {
-        File::append(app_path().'/test.txt',$data['transaction']."-".time().PHP_EOL);
+        $transaction_id = mysql_escape_string($data["transaction"]);
+        
+        Log::Debug("Queue Worker Fired");
+        Log::Debug("Transaction-ID: ".$transaction_id);
+        
+        //Get the transaction from the db
+        $transaction = SDPaymentTransaction::find($transaction_id);
+        if(!$transaction)
+        {
+            Log::Warning("Could not load Transaction from Database");
+            exit();
+        }
+        Log::Debug("Transaction loaded from DB");
+        Log::Debug("Transaction user_id: ".$transaction->user_id);
+        Log::Debug("Transaction payment provider:".$transaction->payment_provider);
+        Log::Debug("Transaction Item ID")
+        //Get the user from the DB
+        
+        //Get the item from the DB
+        
+        //Get the User info from the DB
+        
+        //Confirm that we have got the required infos
+        
+        //Call the actual function
+        
+        //Update the User in the DB
+        
+        //Check for errors
+        
     }
 }
