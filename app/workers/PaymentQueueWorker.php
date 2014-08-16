0a3d56eb502c2f7ae54d807c76006f1a9d784bb3
Werner 'Arrow768 <arrow768@sourcedonates.com>
2014-08-16 20:12:38 +0200

    Updated the queue worker

--- app/workers/PaymentQueueWorker.php
+++ app/workers/PaymentQueueWorker.php
@@ -84,8 +84,8 @@ class PaymentQueueWorker
             }
 
             //Call the item multiple times according to the item count
-            //for ($i = 1; $i = $trans_item->count; $i++)
-            //{
+            for ($i = 1; $i = $trans_item->count; $i++)
+            {
                 foreach ($handlers as $handler)
                 {
                     Log::info("Got Handler Class: " . $handler->class);
@@ -93,9 +93,9 @@ class PaymentQueueWorker
 
                     $item_handler = new $handler->class;
 
-                    $item_handler->add_item($user, $user_infos, $item_handler);
+                    //$item_handler->add_item($user, $user_infos, $handler->params);
                 }
-            //}
+            }
         }
 
 
