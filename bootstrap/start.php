149c46abcd2b55fb8878d59115e129f6330f5e17
Werner 'Arrow768 <arrow768@sourcedonates.com>
2014-09-16 20:14:02 +0200

    Changed the way the db config is handled

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 
 $env = $app->detectEnvironment(array(
-    'development' => array('server.sourcedonates.com'),
+    'development' => array('server.sourcedonates.com','colorful-mist'),
     'local' => array('homestead', '.local')
 ));
 
